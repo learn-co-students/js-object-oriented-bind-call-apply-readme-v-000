2ac497a73ba1ca58007d4a7ee54941fc75a77b9e
Carol Greene <carolgreene3@gmail.com>
2018-09-08 01:51:06 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 // use this to copy code snippets or us your browser's console
-function greet() {
-    console.log(`my name is ${this.name}, hi!`);
+class User {
+	constructor(name, favoriteBand) {
+		this.name = name;
+		this.favoriteBand = favoriteBand;
+	}
+	favoriteBandMatches(bands) {
+		console.log('in User scope: ', this.favoriteBand);
+		return bands.filter(
+			function(band) {
+				console.log('in the anonymous function scope: ', this.favoriteBand);
+				return band == this.favoriteBand;
+			}.bind(this)
+		)[0];
+	}
 }
-greet();
+
+let billy = new User('billy', 'paul simon');
+billy.favoriteBandMatches(['paul simon', 'the kooks']);
