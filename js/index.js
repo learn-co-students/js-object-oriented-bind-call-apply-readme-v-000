1d0dbbec34358fdd91944c5d6bf6312bf92c38b6
Kazumi Karbowski <kazkazinus@gmail.com>
2018-09-11 23:02:59 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-// use this to copy code snippets or us your browser's console
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+greet();
+// my name is , hi!
+
+let person = {
+    name: 'Bob',
+    greet: greet
+};
+
+person.greet();
+
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+
+let sally = { name: 'Sally' };
+
+greet.apply(sally);
+// my name is Sally, hi!
+
+greet.call(sally);
+// my name is Sally, hi!
+
+class User {
+    constructor(name, favoriteBand) {
+        this.name = name;
+        this.favoriteBand = favoriteBand;
+    }
+
+    favoriteBandMatches(bands) {
+        console.log('in User scope: ', this.favoriteBand);
+        return bands.filter(function (band) {
+            console.log('in the anonymous function scope: ', this.favoriteBand);
+            return band == this.favoriteBand;
+        }.bind(this)
+        )[0];
+    }
+}
+
+let billy = new User('billy', 'paul simon');
+billy.favoriteBandMatches(['paul simon', 'the kooks']);
