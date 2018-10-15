ff911980ddd2d92aae1113ac4040866399b3b3ce
hkleindl <hkleindl@gmail.com>
2018-10-14 20:53:48 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // use this to copy code snippets or us your browser's console
+function greet() {
+  console.log(`my name is ${this.name}, hi!`);
+}
+
+let person = {
+  name: 'Bob',
+  greet: greet
+};
