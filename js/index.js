e2f11d572e4c8b61a6a5823fbef27b9546ac31a0
Jennifer Pazos <jmp521@gmail.com>
2018-12-07 00:43:43 -0600

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-// use this to copy code snippets or us your browser's console
+// // use this to copy code snippets or us your browser's console
+// function greet(name1, name2) {
+//   console.log(`my name is ${this.name}, hi!`)
+//   console.log(`Hi ${name1}, I'm ${name2}`)
+// }
+//
+// greet()
+//
+// let person = {
+//   name: "Jen",
+//   greet: greet,
+// }
+//
+// // person.greet()
+// // greet.call(person)
+// greet.apply(person)
+
+
+class Event{
+  constructor(title, keywords) {
+    this.title = title,
+    this.keywords = keywords
+  }
+
+}
+
+class User {
+  constructor(name, interests) {
+    this.name = name,
+    this.interests = interests
+  }
+}
