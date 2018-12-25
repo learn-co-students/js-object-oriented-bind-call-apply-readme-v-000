ebdcc4f2779c453b159df696a8cf290e2c035329
Kachel Wittig <kjwittig@gmail.com>
2018-12-24 23:13:27 -0600

    Done

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
-let sally = { name: "Sally"}
-
-function greet(customer) {
-  console.log(`Hi ${customer}, my name is ${this.name}!`);
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
 }
 
-let newGreet = greet.bind(sally)
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
 
-greet.bind(sally)('Bob')
+    matchInterests(event) {
+      return event.keywords.some(word => this.interests.includes(word));
+    }
+}
+
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+
+billy.matchInterests(freeMusic);
