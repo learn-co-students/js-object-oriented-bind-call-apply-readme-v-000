09a82870fe86d34ac8c8de93ff19ec33b6b32895
Kachel Wittig <kjwittig@gmail.com>
2018-12-24 22:58:46 -0600

    binding

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// use this to copy code snippets or use your browser's console
+let sally = { name: "Sally"}
+
+function greet(customer) {
+  console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+let newGreet = greet.bind(sally)
+
+greet.bind(sally)('Bob')
