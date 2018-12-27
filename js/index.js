8eb957f379a6f76272c3a6c89352b7a2b966aa8f
SMartinSci <sm674@cornell.edu>
2018-12-27 15:06:19 -0800

    Done.

--- js/index.js
+++ js/index.js
@@ -29,3 +29,9 @@ let sally = { name: 'Sally' };
 function greet(customerOne, customerTwo) {
     console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
 }
+
+greet.call(sally, "Terry", "George");
+
+greet.call(sally);
+
+greet.apply(sally, ["Terry", "George"]);
