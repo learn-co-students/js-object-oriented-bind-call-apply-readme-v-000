3847de2fefbcede3abcd0ad4b61972e3a1bb67cd
charlie  lee <ctlee9096@gmail.com>
2018-12-27 03:32:53 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // use this to copy code snippets or use your browser's console
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+
+greet(); // my name is , hi!
+
+let person = {
+    name: 'Bob',
+    greet: greet
+};
+
+person.greet(); // my name is Bob, hi!
+
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+
+let sally = { name: 'Sally' };
+
+greet.call(sally);
+// my name is Sally, hi!
+
+greet.apply(sally);
+// my name is Sally, hi!
+
+let sally = { name: 'Sally' };
+
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+
+greet.call(sally, 'Terry', 'George');
+// Hi Terry and George, my name is Sally!
