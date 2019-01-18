359f60e9878d71465bd981cdf465aba3e498f1a9
Brenden Thornton <thorntonbrenden@gmail.com>
2019-01-18 14:06:34 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // use this to copy code snippets or use your browser's console
+
+let sally = { name: 'Sally' };
+
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+let newGreet = greet.bind(sally); // newGreet is context-bound to sally
+
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+
+greet('Bob');
+// Hi Bob, my name is !
