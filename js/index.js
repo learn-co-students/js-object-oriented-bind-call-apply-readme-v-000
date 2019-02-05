15e9948fe2cd9a407f143d22a013a06c5ac21499
freeWillee <will.ik.lee@gmail.com>
2019-02-05 12:05:55 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
-let sally { name: 'Sally' };
-
-function greet(customer) {
-    console.log(`Hi ${customer}, my name is ${this.name}.`);
+class User {
+	constructor(name, favoriteBand) {
+		this.name = name;
+		this.favoriteBand = favoriteBand;
+	}
+	favoriteBandMatches(bands) {
+		console.log('in User scope: ', this.favoriteBand);
+		return bands.filter(
+			function(band) {
+				console.log('in the anonymous function scope: ', this.favoriteBand);
+				return band == this.favoriteBand;
+			}.bind(this)
+		)[0];
+	}
 }
 
-let newGreet = greet.bind(sally);
-
-newGreet('bob');
-
+let billy = new User('billy', 'paul simon');
+billy.favoriteBandMatches(['paul simon', 'the kooks']);
