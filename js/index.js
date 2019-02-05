a20838cdb01b8005803da95d12bbaf386375cd09
freeWillee <will.ik.lee@gmail.com>
2019-02-05 11:55:06 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// use this to copy code snippets or use your browser's console
+let sally { name: 'Sally' };
+
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}.`);
+}
+
+let newGreet = greet.bind(sally);
+
+newGreet('bob');
+
