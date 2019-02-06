584e652ff485814bb6077b35067036b348145ce7
mrjak13 <justinkenney13@gmail.com>
2019-02-06 06:25:05 -0800

    finished code along

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // use this to copy code snippets or use your browser's console
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+
+let person = {
+    name: 'Bob',
+    greet: greet
+};
+
+let sally = { name: 'Sally' };
