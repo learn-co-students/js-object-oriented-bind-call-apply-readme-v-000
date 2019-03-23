498d0240df03ab024ed3fdd4b2d1fbba6983dbc6
Jonathan <jonathanyi81@gmail.com>
2019-03-23 13:49:10 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,67 @@
 // use this to copy code snippets or use your browser's console
+// function greet() {
+//   console.log(`my name is ${this.name}, hi!`);
+// }
+//
+// greet();
+//
+// let person = {
+//   name: "Bob",
+//   greet: greet
+// };
+//
+// person.greet();
+//
+// let sally = { name: "Sally" };
+//
+// greet.call(sally);
+// greet.apply(sally);
+
+// function greet(customerOne, customerTwo) {
+//   console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+// }
+//
+// let sally = { name: "Sally" };
+//
+// greet.call(sally, "Terry", "George");
+// greet.call(sally);
+//
+// greet.apply(sally, ["Terry", "George"]);
+
+// let sally = { name: "Sally" };
+//
+// function greet(customer) {
+//   console.log(`Hi ${customer}, my name is ${this.name}!`);
+// }
+//
+// let newGreet = greet.bind(sally);
+//
+// newGreet("Bob");
+// greet("Bob");
+// greet.bind(sally)("Bob");
+
+class Event {
+  constructor(title, keywords) {
+    this.title = title;
+    this.keywords = keywords;
+  }
+}
+
+class User {
+  constructor(name, interests) {
+    this.name = name;
+    this.interests = interests;
+  }
+
+  matchInterests(event) {
+    return event.keywords.some(
+      function(word) {
+        return this.interests.includes(word);
+      }.bind(this) // added to the and of the callback function
+    );
+  }
+}
+
+let billy = new User("billy", ["music", "art", "movies"]);
+let freeMusic = new Event("Free Music Show", ["music", "free", "outside"]);
+billy.matchInterests(freeMusic);
