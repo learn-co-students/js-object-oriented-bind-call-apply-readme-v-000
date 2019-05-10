2d1eeec520e1ca1914aa6f584a1d2e3f6f58842b
Guess <guess@Guesss-iMac.local>
2019-05-09 21:33:07 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
 
-// Uncaught TypeError: Cannot read property 'interests' of undefined
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+
+    matchInterests(event) {
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
+    }
+}
+
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+
+billy.matchInterests(freeMusic);
