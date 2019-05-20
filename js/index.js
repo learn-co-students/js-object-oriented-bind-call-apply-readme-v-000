c9182a899e7694abddde063a5c13b115b1bcccbc
Katie Moosman <moosman.katie@gmail.com>
2019-05-20 16:46:42 -0600

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,64 @@
 // use this to copy code snippets or use your browser's console
+let sally = { name: 'Sally' };
+ 
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+ 
+greet.call(sally, 'Terry', 'George');
+
+greet.apply(sally, ['Terry', 'George']);
+
+
+//binding
+
+let sally = { name: 'Sally' };
+ 
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+ 
+let newGreet = greet.bind(sally); // newGreet is context-bound to sally
+ 
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+ 
+greet('Bob');
+
+greet.bind(sally)('Bob');
+// Hi Bob, my name is Sally!
+
+
+
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        return event.keywords.some(
+            function(word) {
+            return this.interests.includes(word);
+        }.bind(this)
+    );
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
+
+
+//arrow functions 
+matchInterests(event) {
+    return event.keywords.some(word => this.interests.includes(word));
+  }
