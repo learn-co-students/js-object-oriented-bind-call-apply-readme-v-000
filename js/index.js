eba2a0811149d25ca3a3d25f70ca860f54d48e61
eryzna <eryzna@gmail.com>
2021-02-24 10:03:58 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// use this to copy code snippets or use your browser's console
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+ 
+let sally = { name: 'Sally' };
+ 
+greet.call(sally);
+// my name is Sally, hi!
+ 
+greet.apply(sally);
+// my name is Sally, hi!
