7a237f9aded5f3c40a185b3be3b033181a8ea11f
epic0448 <samuelogundiran@Yahoo.com>
2021-03-13 18:58:16 -0500

    index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
-// use this to copy code snippets or use your browser's console
 function greet() {
     console.log(`my name is ${this.name}, hi!`);
 }
  
-greet(); // my name is , hi!
  
-let person = {
-    name: 'Bob',
-    greet: greet
-};
+let sally = { name: 'Bob'};
  
-person.greet();
+greet.call(sally);
+
+greet.apply(sally)
